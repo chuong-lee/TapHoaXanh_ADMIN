89345eb30a790b647cb6e6c99f379a8ef6a38905
phongndps37996 <phongndps37996@gmail.com>
2025-08-25 20:19:12 +0700

    fix url /api

--- src/app/lib/axios.ts
+++ src/app/lib/axios.ts
@@ -14,7 +14,7 @@ function onRefreshed(token: string) {
 
 const api = axios.create({
   // baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000",
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://taphoaxanh-be.vercel.app",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://taphoaxanh-be.vercel.app/api/",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
